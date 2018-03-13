6e706e6103b331d204a16cdc8d0ccc7a3a8d2763
Rob Harris <roharris@redhat.com>
2018-03-13 11:01:58 +0000

    started implementation of review table

---
+++ reviewTableView.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML>
+<!--
+	Industrious by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Pathfinder</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+
+		<script>
+	  $( function() {
+	    $( "#speed" ).selectmenu();
+
+	    $( "#files" ).selectmenu();
+
+	    $( "#number" )
+	      .selectmenu()
+	      .selectmenu( "menuWidget" )
+	        .addClass( "overflow" );
+
+	    $( "#salutation" ).selectmenu();
+	  } );
+	  </script>
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<a class="logo" href="index.php">Pathfinder</a>
+				<nav>
+					<a href="#menu">Menu</a>
+				</nav>
+			</header>
+
+    <div class="table-responsive">  
+ 
+        <table id="table_id" class="display">
+            <thead>  
+                <tr>  
+                    <td><b>Application Name</b></td>  
+                    <td><b>Decision</b></td>  
+                    <td><b>Estimate</b></td>  
+                    <td><b>Business Priority</b></td>  
+                </tr>  
+            </thead>  
+            <tr>
+                <td>My App 1</td>
+                <td>Refactor</td>
+                <td>Large</td>
+                <td>6</td>
+            </tr>
+            <tr>
+                <td>My App 2</td>
+                <td>Retire</td>
+                <td>Large</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>My App 3</td>
+                <td>Rehost</td>
+                <td>Small</td>
+                <td>9</td>
+            </tr>
+        </table>
+    </div>
+
+
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+         
+
+
+	</body>
+</html>
