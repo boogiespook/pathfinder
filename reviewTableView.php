4d2f1c508a66f23082080027e9299bb961a9344b
Rob Harris <roharris@redhat.com>
2018-03-14 17:50:51 +0000

    Refactored getting review data using new API path

--- reviewTableView.php
+++ reviewTableView.php
@@ -22,81 +22,9 @@ $custId = $_REQUEST['customer'];
 
     <script type="text/javascript">
 
-
-    function getRequestApp(url) {
-        var xhttp = new XMLHttpRequest();
-        console.log(url);
-        let data = null;
-
-        xhttp.open("GET", url, true);
-        xhttp.setRequestHeader("Content-type", "application/json");
-        xhttp.onreadystatechange = function() {
-            if (this.readyState === 4 && this.status === 200) {
-            data = JSON.parse(this.responseText);
-            console.log("data is 8 ");
-            console.log(data);
-            sortApplicationIds(data);
-            }
-        }
-        xhttp.send();
-    }
-
-
-    function sortApplicationIds(data){
-        console.log("data length " + data.length);
-        var appsAndId = [];
-        // var xhr = [];
-        for (let i = 0; i < data.length; i++){
-            if (data[i]["Review"] != null){
-                appsAndId.push([data[i]["Name"], data[i]["Id"], data[i]["Review"]]);
-                // console.log("apps and id: ", appsAndId);
-            }
-        }
-        console.log("apps and id: ", appsAndId);
-        getRequestReview(appsAndId);
-    }
-
-
-    function getRequestReview(appsAndId) {
-        let xhttp = new XMLHttpRequest();
-        let customerId = '<?php print $custId; ?>';
-        let data = null;
-        let applicationName = '';
-        // console.log(appsAndId);
-
-        dataSet.pop()
-
-
-        let xhr = [];
-        for (let i = 0; i < appsAndId.length; i++){
-            let applicationName = appsAndId[i][0];
-            let applicationId = appsAndId[i][1];
-            let reviewId = appsAndId[i][2];
-            let url = "http://pathtest-pathfinder.6923.rh-us-east-1.openshiftapps.com/api/pathfinder/customers/" + String(customerId) + "/applications/" + applicationId + "/review/" + reviewId + "/";
-            xhr[i] = new XMLHttpRequest();
-            xhr[i].open("GET", url, true);
-            xhr[i].setRequestHeader("Content-type", "application/json");
-            xhr[i].onreadystatechange = function() {
-                if (this.readyState === 4 && this.status === 200) {
-                    let data = JSON.parse(this.responseText);
-                    console.log("return  ", data);
-                    dataSet.push([applicationName, data["ReviewDecision"]["rank"], data["WorkEffort"]["rank"], data["BusinessPriority"]]);
-                    sortReviewData(dataSet);
-                }
-            }
-            xhr[i].send();
-        }
-        
-
-    }
-
     function sortReviewData(dataBoi) {
-        console.log("full data set ius ");
-        console.log(dataBoi);
-
-
-
-
+        //console.log("full data set ius ");
+        // console.log(dataBoi);
         
         $(document).ready(function() {
         $('#reviewTable').DataTable( {
@@ -104,31 +32,48 @@ $custId = $_REQUEST['customer'];
         retrieve: true,
         paging: false,
         columns: [
-            { title: "Application" },
-            { title: "Decision" },
-            { title: "Estimate" },
+            { title: "Application Name" },
+            { title: "Review Decision" },
+            { title: "Effort Estimate" },
             { title: "Business Priority" },
+            { title: "Work Priority" },
+
         ]
     } );
 } );
 
     }
 
+    
 
-    function getApplications() {
+    function getReviews(){
         let customerId = '<?php print $custId; ?>';
-        let url = "http://pathtest-pathfinder.6923.rh-us-east-1.openshiftapps.com/api/pathfinder/customers/" + String(customerId) + "/applications/";
-        getRequestApp(url);
+        let url = "http://pathtest-pathfinder.6923.rh-us-east-1.openshiftapps.com/api/pathfinder/customers/" + String(customerId) + "/reviews";
+        var xhttp = new XMLHttpRequest();
+        // console.log(url);
+        let data = null;
+        let dataSet = []
+
+
+        xhttp.open("GET", url, true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                data = JSON.parse(this.responseText);
+                console.log(data);
+                for (let i = 0; i < data.length; i++){
+                    dataSet.push([data[i]["AssessmentId"], data[i]["ReviewDecision"]["rank"], data[i]["WorkEffort"]["rank"], data[i]["BusinessPriority"], data[i]["WorkPriority"]]);
+                }
+            sortReviewData(dataSet);
+            }
+        }
+        xhttp.send();
 
     }
 
-    var dataSet = [
-         [ "", "", "", ""],
-     ];
 
-        
-    // Launches JS scripts
-    getApplications();
+    getReviews();
+
 
 </script>
 
